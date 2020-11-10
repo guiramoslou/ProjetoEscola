52e2dd9035e1724936b36254d3f4318a6d65975a
Guilherme Ramos Lourenco <guiramoslou@gmail.com>
2020-11-10 10:57:02 -0300

    Tratamento de exceções para classe MentorController

--- demo/src/main/java/com/example/demo/controller/MentorController.java
+++ demo/src/main/java/com/example/demo/controller/MentorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -18,28 +19,45 @@ public class MentorController {
 
     @GetMapping
     public ResponseEntity<List<MentorDTO>> getMentores() {
-        return ResponseEntity.ok(mentorService.getMentores());
+        try {
+            return new ResponseEntity(mentorService.getMentores(), HttpStatus.OK);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Something went wrong", exception);
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<MentorDTO> getMentorById(@PathVariable Long id) {
-        return ResponseEntity.ok(mentorService.getMentorById(id).get());
+        return mentorService.getMentorById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<MentorDTO> createMentor(@RequestBody MentorDTO mentorDTO) {
-        return ResponseEntity.ok(mentorService.createMentor(mentorDTO));
+        try {
+            return new ResponseEntity<MentorDTO>(mentorService.createMentor(mentorDTO), HttpStatus.CREATED);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid parameters", exception);
+        }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<MentorDTO> updateMentor(@RequestBody MentorDTO mentorDTO, @PathVariable Long id) {
-        return ResponseEntity.ok(mentorService.updateMentor(mentorDTO, id));
+        try {
+            return new ResponseEntity<MentorDTO>(mentorService.updateMentor(mentorDTO, id), HttpStatus.ACCEPTED);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid parameters", exception);
+        }
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void deleteMentor(@PathVariable Long id) {
-        mentorService.deleteMentor(id);
+    public ResponseEntity deleteMentor(@PathVariable Long id) {
+        try {
+            mentorService.deleteMentor(id);
+            return ResponseEntity.accepted().build();
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id not found", exception);
+        }
     }
-
 }
